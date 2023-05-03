544ff30556fa021e603cc27aa4d5018327b80fa6
Ben Sampson <ben.sampson@nationalarchives.gov.uk>
2023-05-03 15:13:33 +0100

    chore: WIP commit

--- build.sbt
+++ build.sbt
@@ -7,3 +7,8 @@ libraryDependencies += "io.cucumber" % "cucumber-junit" % "7.11.2" % Test
 libraryDependencies += "com.novocode" % "junit-interface" % "0.11" % Test
 libraryDependencies += "software.amazon.awssdk" % "s3" % "2.20.20"
 libraryDependencies += "software.amazon.awssdk" % "sns" % "2.20.20"
+libraryDependencies ++= Seq(
+  "org.slf4j" % "slf4j-api" % "2.0.7",
+  "org.slf4j" % "slf4j-simple" % "2.0.7"
+)
+libraryDependencies += "com.jayway.jsonpath" % "json-path" % "2.8.0"
--- src/main/scala/HelloWorld.scala
+++ src/main/scala/HelloWorld.scala
@@ -12,34 +12,101 @@ import software.amazon.awssdk.services.sns.model.PublishRequest
 import software.amazon.awssdk.services.sns.model.PublishResponse
 import software.amazon.awssdk.services.sns.model.SnsException
 import software.amazon.awssdk.services.sns.model.InvalidParameterException
+import software.amazon.awssdk.services.s3.model.ListObjectsRequest
+import software.amazon.awssdk.services.s3.model.ListObjectsResponse
+import software.amazon.awssdk.services.s3.model.S3Object
+import software.amazon.awssdk.services.s3.model.GetObjectResponse
+import software.amazon.awssdk.services.s3.model.GetObjectRequest
+import software.amazon.awssdk.core.ResponseBytes
+import scala.jdk.CollectionConverters._
+import com.jayway.jsonpath.JsonPath
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 def bucketExists(url: String): Boolean =
-  val credentialsProvider: ProfileCredentialsProvider =
-    ProfileCredentialsProvider.create()
-  val region: Region = Region.EU_WEST_2
   val s3: S3Client = S3Client
     .builder()
-    .region(region)
-    .credentialsProvider(credentialsProvider)
+    .region(Region.EU_WEST_2)    .credentialsProvider(ProfileCredentialsProvider.create())
     .build()
+
   try s3.headBucket(HeadBucketRequest.builder().bucket(url).build())
   catch
     case e: NoSuchBucketException =>
       println(s"Bucket does not exists")
-      false
+      return false
     case e: S3Exception =>
       println(s"Failed with status code = ${e.statusCode()}")
     case e: SdkClientException => println(s"Caught SDK Execption ${e}")
   finally s3.close()
   true
 
-def publishMessageToSnsTopic(message: String, topic: String): Unit =
-  println(s"Got ${message} and ${topic}")
+/** Get the newest message from S3 from the path
+  * JudgmentPackageAvailableMessage/PRODUCER/REFERENCE Assume that the newest
+  * record is the newest object containing the last JSON object
+  * @param producer
+  *   A RequestJudgmentParse producer
+  * @param reference
+  *   A RequestJudgmentParse reference
+  * @bucketName
+  *   A bucket to get the message from
+  * @return
+  *   a JSON String which is the message
+  */
+def getNewestJudgmentPackageAvailableMessage(
+    producer: String,
+    reference: String,
+    bucketName: String
+): String =
+  val objects = listBucket(
+    bucketName,
+    "judgmentpackage.available.JudgmentPackageAvailable/" + producer + "/" + reference
+  )
+  // Get the newest one, get the key (object name) and fetch the object
+  val newestObjectKey = objects.sortBy(_.lastModified()).last.key()
+  getObjectAsString(bucketName, newestObjectKey)
 
-  // Build sns client
-  // val credentialsProvider: ProfileCredentialsProvider =
-  ProfileCredentialsProvider.create()
-  // val region: Region = Region.EU_WEST_2
+/** Return the contents of a buckets
+  * @param bucketName
+  * @param path
+  *   path (or prefix) to get. Defaults to ""
+  * @return
+  *   Scala list of S3Objects
+  */
+def listBucket(bucketName: String, path: String = ""): List[S3Object] =
+  val s3: S3Client = S3Client
+    .builder()
+    .region(Region.EU_WEST_2)
+    .credentialsProvider(ProfileCredentialsProvider.create())
+    .build()
+  val listObjects: ListObjectsRequest =
+    ListObjectsRequest.builder().bucket(bucketName).prefix(path).build()
+  val res: ListObjectsResponse = s3.listObjects(listObjects)
+  res.contents().asScala.toList
+
+/** Return an object in a bucket as a string
+  * @param bucketName
+  * @param key
+  * @return
+  *   The object as UTF8 encoded string
+  */
+def getObjectAsString(bucketName: String, key: String): String =
+  val s3: S3Client = S3Client
+    .builder()
+    .region(Region.EU_WEST_2)
+    .credentialsProvider(ProfileCredentialsProvider.create())
+    .build()
+
+  val getObjectRequest: GetObjectRequest =
+    GetObjectRequest.builder().bucket(bucketName).key(key).build()
+
+  val res: ResponseBytes[GetObjectResponse] =
+    s3.getObjectAsBytes(getObjectRequest)
+  res.asUtf8String()
+
+def publishMessageToSnsTopic(message: String, topic: String): Unit =
+  val logger: Logger = LoggerFactory.getLogger("NameofApp")
+  logger.info(s"Got message ${message}")
+
   val sns: SnsClient = SnsClient
     .builder()
     .region(Region.EU_WEST_2)
@@ -62,7 +129,7 @@ def publishMessageToSnsTopic(message: String, topic: String): Unit =
   finally sns.close()
 
 @main def commandLine(command: String, args: String*): Unit =
-
+  val logger: Logger = LoggerFactory.getLogger("NameofApp")
   var a: String = """
 {
   "properties" : {
@@ -70,25 +137,27 @@ def publishMessageToSnsTopic(message: String, topic: String): Unit =
     "timestampMillis" : "2023-03-29T11:00:12.280Z",
     "function" : "fcl-judgment-parse-request",
     "producer" : "FCL",
-    "executionId" : "db8d202b-c174-4066-a482-b39fa52cbd8a",
+    "executionId" : "db8d202b--c174-4066-a482-b39fa52cbd8a",
     "parentExecutionId" : null
   },
   "parameters": {
-    "reference": "FCL-BS-24",
+    "reference": "FCL-BS-26",
     "judgmentURI": "https://tna-caselaw-assets.s3.eu-west-2.amazonaws.com/eat/2022/1/eat_2022_1.docx",
     "originator" : "FCL"
   }
 }
 """
 
-  println(s"Got command = ${command} with ${args}")
+  logger.info(s"Got command = ${command} with ${args}")
   val argsArray = args.toArray
 
   command match
     case "bucket" => bucketExists(args.head)
     // case "snsPublish" => publishMessageToSnsTopic(argsArray(0), argsArray(1))
     case "snsPublish" => publishMessageToSnsTopic(a, argsArray(1))
-    case _            => println(s"Unknown command ${command}")
+    // case "log" => testLogging()
+    // case "listBucket" => listBucket("pte-bs-tre-tre-out-capture")
+    case _ => println(s"Unknown command ${command}")
 
 // println(s"Got ${everything}")
 //case
